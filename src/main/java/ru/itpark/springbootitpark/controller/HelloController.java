76fb0142286d4c3afa987a1817189cf3c9076e01
coursar <coursar@ya.ru>
2018-12-30 00:14:59 +0300

    Remove unused imports

--- src/main/java/ru/itpark/springbootitpark/controller/HelloController.java
+++ src/main/java/ru/itpark/springbootitpark/controller/HelloController.java
@@ -3,7 +3,6 @@ package ru.itpark.springbootitpark.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
